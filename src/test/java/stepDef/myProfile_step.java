18e581ffedbd797c10172f86df811333ca032c79
shimanahar <shima.nahar19@gmail.com>
2021-10-13 20:28:21 -0400

    fww2

---
+++ src/test/java/stepDef/myProfile_step.java
@@ -0,0 +1,9 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+
+public class myProfile_step {
+    @Then("I should be able to successfully log in")
+    public void iShouldBeAbleToSuccessfullyLogIn() {
+    }
+}
